c54b407b21e17868303b096283bcb9a88058207f
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-07-06 14:26:16 -0700

    Carga inicial de proyecto

---
+++ src/main/java/itson/proyectografosguanajuato/ProyectoGrafosGuanajuato.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.proyectografosguanajuato;
+
+/**
+ *
+ * @author pedro
+ */
+public class ProyectoGrafosGuanajuato {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
